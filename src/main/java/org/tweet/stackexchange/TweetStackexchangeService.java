c34f6825c63d90af40aef9ff0fabf2a15210f24b
eugenp <hanriseldon@gmail.com>
2013-02-13 15:29:21 +0200

    spring social and persistence work

--- src/main/java/org/tweet/stackexchange/TweetStackexchangeService.java
+++ src/main/java/org/tweet/stackexchange/TweetStackexchangeService.java
@@ -1,10 +1,15 @@
 package org.tweet.stackexchange;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.tweet.twitter.TwitterService;
 
 @Service
 public class TweetStackexchangeService {
+
+    @Autowired
+    private TwitterService twitterService;
 
     public TweetStackexchangeService() {
         super();
