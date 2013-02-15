8f15b42153af82e6f35e1c680eb916a90a2079d8
eparaschiv <eparaschiv@optaros.com>
2013-02-15 13:35:42 +0200

    persistence work, api work

--- src/main/java/org/tweet/spring/SocialConfig.java
+++ src/main/java/org/tweet/spring/SocialConfig.java
@@ -1,21 +1,12 @@
 package org.tweet.spring;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.social.connect.ConnectionFactoryLocator;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.connect.UsersConnectionRepository;
-import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
-import org.springframework.social.connect.support.ConnectionFactoryRegistry;
-import org.springframework.social.connect.web.ConnectController;
 import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.connect.TwitterConnectionFactory;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 /**
  * Spring Social Configuration.
@@ -26,16 +17,13 @@ import org.springframework.social.twitter.connect.TwitterConnectionFactory;
 @ComponentScan({ "org.tweet.twitter" })
 public class SocialConfig {
 
-    public static final String CONSUMER_KEY = "2jXUgs9QmrNzdzRzglDBTg";
-    public static final String CONSUMER_SECRET = "XipjDslY6bqhulUW0IHbAEIqHgkk0IWOfwg4OljU";
-    public static final String ACCESS_TOKEN = "1169459593-tWD1l9Ni7Ocy3EdcHkM0WF7SGfbpXsbX9NAAi3f";
-    public static final String ACCESS_TOKEN_SECRET = "2zjY7iPCm8PDcPfrQvKjdU5No3SXgkuluncqYgBBc";
+    private static final String CONSUMER_KEY = "2jXUgs9QmrNzdzRzglDBTg";
+    private static final String CONSUMER_SECRET = "XipjDslY6bqhulUW0IHbAEIqHgkk0IWOfwg4OljU";
+    private static final String ACCESS_TOKEN = "1169459593-tWD1l9Ni7Ocy3EdcHkM0WF7SGfbpXsbX9NAAi3f";
+    private static final String ACCESS_TOKEN_SECRET = "2zjY7iPCm8PDcPfrQvKjdU5No3SXgkuluncqYgBBc";
 
     @Autowired
     private Environment environment;
-
-    @Autowired
-    private DataSource dataSource;
 
     public SocialConfig() {
         super();
@@ -44,30 +32,9 @@ public class SocialConfig {
     // API
 
     @Bean
-    public ConnectionFactoryLocator connectionFactoryLocator() {
-        final ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
-        registry.addConnectionFactory(new TwitterConnectionFactory(CONSUMER_KEY, CONSUMER_SECRET));
-        return registry;
-    }
-
-    @Bean
-    public UsersConnectionRepository usersConnectionRepository() {
-        return new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator(), Encryptors.noOpText());
-    }
-
-    @Bean
-    public ConnectionRepository connectionRepository() {
-        return usersConnectionRepository().createConnectionRepository("ServerFaultBest");
-    }
-
-    @Bean
-    public ConnectController connectController() {
-        return new ConnectController(connectionFactoryLocator(), connectionRepository());
-    }
-
-    @Bean
     public Twitter twitter() {
-        return connectionRepository().getPrimaryConnection(Twitter.class).getApi();
+        final TwitterTemplate twitterTemplate = new TwitterTemplate(SocialConfig.CONSUMER_KEY, SocialConfig.CONSUMER_SECRET, SocialConfig.ACCESS_TOKEN, SocialConfig.ACCESS_TOKEN_SECRET);
+        return twitterTemplate;
     }
 
     // http://stackoverflow.com/questions/7968641/spring-social-twitter-oauth
--- src/main/java/org/tweet/stackexchange/TweetStackexchangeService.java
+++ src/main/java/org/tweet/stackexchange/TweetStackexchangeService.java
@@ -1,15 +1,26 @@
 package org.tweet.stackexchange;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.stackexchange.api.client.QuestionsApi;
 import org.tweet.twitter.TwitterService;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 @Service
 public class TweetStackexchangeService {
 
     @Autowired
     private TwitterService twitterService;
+
+    @Autowired
+    private QuestionsApi questionsApi;
 
     public TweetStackexchangeService() {
         super();
@@ -19,8 +30,11 @@ public class TweetStackexchangeService {
 
     // 6 hours
     @Scheduled(fixedRate = 21600000)
-    public void tweetStackExchangeTopQuestion() {
-        //
+    public void tweetStackExchangeTopQuestion() throws JsonProcessingException, IOException {
+        final String questionsAsJson = questionsApi.questions(100);
+        final ObjectMapper mapper = new ObjectMapper();
+        final JsonNode rootNode = mapper.readTree(questionsAsJson);
+        final ArrayNode questionsArray = (ArrayNode) rootNode.get("items");
     }
 
 }
