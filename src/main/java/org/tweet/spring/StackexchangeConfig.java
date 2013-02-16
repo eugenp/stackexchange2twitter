50d23ae3e38e846fdb7015ea3c80ba5966cdc86a
eugenp <hanriseldon@gmail.com>
2013-02-16 19:56:04 +0200

    multiple maven and spring fixes

--- src/main/java/org/tweet/spring/StackexchangeConfig.java
+++ src/main/java/org/tweet/spring/StackexchangeConfig.java
@@ -3,8 +3,10 @@ package org.tweet.spring;
 import org.apache.http.impl.client.DecompressingHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.stackexchange.api.client.QuestionsApi;
 
+@Configuration
 public class StackexchangeConfig {
 
     @Bean
