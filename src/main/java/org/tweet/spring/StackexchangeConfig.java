d1b6adbb6390ce286e0d99a5ae954b0a1c5632f6
eparaschiv <eparaschiv@optaros.com>
2013-02-18 13:13:19 +0200

    changing the connection manager

--- src/main/java/org/tweet/spring/StackexchangeConfig.java
+++ src/main/java/org/tweet/spring/StackexchangeConfig.java
@@ -2,6 +2,8 @@ package org.tweet.spring;
 
 import org.apache.http.impl.client.DecompressingHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.impl.conn.SchemeRegistryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.stackexchange.api.client.QuestionsApi;
@@ -11,7 +13,13 @@ public class StackexchangeConfig {
 
     @Bean
     public QuestionsApi questionsApi() {
-        return new QuestionsApi(new DecompressingHttpClient(new DefaultHttpClient()));
+        final PoolingClientConnectionManager cxMgr = new PoolingClientConnectionManager(SchemeRegistryFactory.createDefault());
+        cxMgr.setMaxTotal(100);
+        cxMgr.setDefaultMaxPerRoute(20);
+
+        final DefaultHttpClient rawHttpClient = new DefaultHttpClient(cxMgr);
+        final DecompressingHttpClient httpClient = new DecompressingHttpClient(rawHttpClient);
+        return new QuestionsApi(httpClient);
     }
 
 }
