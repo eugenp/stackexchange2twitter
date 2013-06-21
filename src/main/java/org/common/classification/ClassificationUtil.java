8192e8514afa73ca7c7ee8264cdb2cc59f2524ca
Eugen <eparaschiv@optaros.com>
2013-06-21 17:13:40 +0300

    finished initial classification example

--- src/main/java/org/common/classification/ClassificationUtil.java
+++ src/main/java/org/common/classification/ClassificationUtil.java
@@ -26,6 +26,7 @@ public final class ClassificationUtil {
 
     public static final String COMMERCIAL = "commercial";
     public static final String NONCOMMERCIAL = "noncommercial";
+    private static final int NUMBER_OF_CATEGORIES = 2;
 
     private ClassificationUtil() {
         throw new AssertionError();
@@ -37,7 +38,7 @@ public final class ClassificationUtil {
      * The following are encoded: the type of content and the actual bag of words
      * The label argument to the NamedVector is either “commercial” or “non-commercial”
      */
-    public static Vector encode(final String type, final Iterable<String> words) throws IOException {
+    public static NamedVector encode(final String type, final Iterable<String> words) throws IOException {
         final FeatureVectorEncoder content_encoder = new AdaptiveWordValueEncoder("content");
         content_encoder.setProbes(2);
 
@@ -50,7 +51,7 @@ public final class ClassificationUtil {
         for (final String word : words) {
             content_encoder.addToVector(word, vect);
         }
-        return new NamedVector(vect, "label");
+        return new NamedVector(vect, type);
     }
 
     public static VectorWriter loadData(final String pathOnDisk) throws IOException {
@@ -77,16 +78,18 @@ public final class ClassificationUtil {
         final VectorWritable value = new VectorWritable();
         while (reader.next(new LongWritable(), value)) {
             final NamedVector v = (NamedVector) value.get();
-            reg.train("spam".equals(v.getName()) ? 1 : 0, v);
+            reg.train(COMMERCIAL.equals(v.getName()) ? 1 : 0, v);
         }
         reg.close();
     }
 
-    public static void trainClassifier(final Iterable<NamedVector> vectors) throws IOException {
-        final AdaptiveLogisticRegression reg = new AdaptiveLogisticRegression(2, FEATURES, new L1());
+    public static AdaptiveLogisticRegression trainClassifier(final Iterable<NamedVector> vectors) throws IOException {
+        final AdaptiveLogisticRegression reg = new AdaptiveLogisticRegression(NUMBER_OF_CATEGORIES, FEATURES, new L1());
         for (final NamedVector vect : vectors) {
             reg.train(COMMERCIAL.equals(vect.getName()) ? 1 : 0, vect);
         }
+        reg.close();
+        return reg;
     }
 
 }
--- src/test/java/org/common/classification/ClassificationUnitTest.java
+++ src/test/java/org/common/classification/ClassificationUnitTest.java
@@ -1,14 +1,24 @@
 package org.common.classification;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.common.classification.ClassificationUtil.COMMERCIAL;
+import static org.common.classification.ClassificationUtil.NONCOMMERCIAL;
+import static org.common.classification.ClassificationUtil.encode;
+import static org.common.classification.ClassificationUtil.loadData;
+import static org.common.classification.ClassificationUtil.readBackData;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.SequenceFile.Reader;
+import org.apache.mahout.classifier.sgd.AdaptiveLogisticRegression;
+import org.apache.mahout.classifier.sgd.CrossFoldLearner;
+import org.apache.mahout.classifier.sgd.ModelSerializer;
+import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.NamedVector;
 import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.VectorWritable;
@@ -20,41 +30,38 @@ import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 
 public class ClassificationUnitTest {
-    private static final String FILE_ON_DISK = "file:/tmp/spammery.seq";
+    private static final String VECTOR_FILE_ON_DISK = "file:/tmp/spammery.seq";
+    private static final String CLASSIFIER_FILE_ON_DISK = "/tmp/classif.ier";
 
     // tests
 
     @Test
     public final void whenTextIsEncodedAsVector2_thenNoExceptions() throws IOException {
-        ClassificationUtil.encode(randomAlphabetic(4), Lists.newArrayList(randomAlphabetic(5), randomAlphabetic(4)));
+        encode(randomAlphabetic(4), Lists.newArrayList(randomAlphabetic(5), randomAlphabetic(4)));
     }
 
     @Test
     public final void whenLoadingClassificationData_thenNoExceptions() throws IOException {
-        ClassificationUtil.loadData(FILE_ON_DISK);
+        loadData(VECTOR_FILE_ON_DISK);
     }
 
     @Test
     public final void whenLoadingClassificationData_thenOperationCorrect() throws IOException {
-        assertNotNull(ClassificationUtil.loadData(FILE_ON_DISK));
+        assertNotNull(loadData(VECTOR_FILE_ON_DISK));
     }
 
     @Test
     public final void givenDataIsLoaded_whenWriterIsUsed_thenNoExceptions() throws IOException {
-        final Vector noncommercial1 = ClassificationUtil.encode(ClassificationUtil.NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("How to travel around the world for a year http://blog.alexmaccaw.com/how-to-travel-around-the-world-for-a-year/"));
-        final Vector noncommercial2 = ClassificationUtil.encode(ClassificationUtil.NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("What is nux and what's it used for? - http://askubuntu.com/questions/18413/what-is-nux-and-whats-it-used-for"));
-        final Vector commercial1 = ClassificationUtil.encode(ClassificationUtil.COMMERCIAL,
-                Splitter.on(CharMatcher.anyOf(" ")).split("We're looking to #hire a Front End Developer/Creative Designer to join our team in Leeds. Get in touch for more information."));
-        final Vector commercial2 = ClassificationUtil.encode(ClassificationUtil.COMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("New job Nurse Practitioners & Physician Assistants - New Jersey http://goo.gl/fb/K8jZP  #hire #jobs"));
+        final List<Vector> vectors = vectors();
 
-        final VectorWriter vectorWriter = ClassificationUtil.loadData(FILE_ON_DISK);
-        vectorWriter.write(Lists.newArrayList(noncommercial1, commercial1, noncommercial2, commercial2));
+        final VectorWriter vectorWriter = loadData(VECTOR_FILE_ON_DISK);
+        vectorWriter.write(vectors);
         vectorWriter.close();
     }
 
     @Test
     public final void givenDataWasWritten_whenDataIsReadBack_thenNoExceptions() throws IOException {
-        final Reader reader = ClassificationUtil.readBackData(FILE_ON_DISK);
+        final Reader reader = readBackData(VECTOR_FILE_ON_DISK);
 
         final LongWritable key = new LongWritable();
         final VectorWritable value = new VectorWritable();
@@ -66,15 +73,15 @@ public class ClassificationUnitTest {
 
     @Test
     public final void whenVectorIsCreatedWrittenAndReadBack_theVectorRemainsTheSame() throws IOException {
-        final Vector originalVector = ClassificationUtil.encode(ClassificationUtil.NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("How to travel around the world for a year http://blog.alexmaccaw.com/how-to-travel-around-the-world-for-a-year/"));
+        final Vector originalVector = encode(NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("How to travel around the world for a year http://blog.alexmaccaw.com/how-to-travel-around-the-world-for-a-year/"));
 
         // write
-        final VectorWriter vectorWriter = ClassificationUtil.loadData(FILE_ON_DISK);
+        final VectorWriter vectorWriter = loadData(VECTOR_FILE_ON_DISK);
         vectorWriter.write(originalVector);
         vectorWriter.close();
 
         // read back
-        final Reader reader = ClassificationUtil.readBackData(FILE_ON_DISK);
+        final Reader reader = readBackData(VECTOR_FILE_ON_DISK);
         final VectorWritable value = new VectorWritable();
         reader.next(new LongWritable(), value);
 
@@ -82,4 +89,73 @@ public class ClassificationUnitTest {
         final NamedVector retrievedVector = (NamedVector) value.get();
         assertThat(retrievedVector, equalTo(originalVector));
     }
+
+    // training
+
+    @Test
+    public final void whenClassifierIsTrained_thenNoExceptions() throws IOException {
+        final List<NamedVector> vectors = namedVectors();
+
+        ClassificationUtil.trainClassifier(vectors);
+    }
+
+    @Test
+    public final void givenClassifierWasTrained_whenPersistedToDisk_thenNoExceptions() throws IOException {
+        final List<NamedVector> vectors = namedVectors();
+        final AdaptiveLogisticRegression classifier = ClassificationUtil.trainClassifier(vectors);
+
+        ModelSerializer.writeBinary(CLASSIFIER_FILE_ON_DISK, classifier.getBest().getPayload().getLearner());
+    }
+
+    // usage
+
+    @Test
+    public final void givenClassifierWasTrained_whenUsingThePersistedToDisk_thenNoExceptions() throws IOException {
+        final List<NamedVector> vectors = namedVectors();
+        final AdaptiveLogisticRegression classifier = ClassificationUtil.trainClassifier(vectors);
+        final CrossFoldLearner bestLearner = classifier.getBest().getPayload().getLearner();
+
+        int correct = 0;
+        int total = 0;
+        for (final NamedVector vect : vectors) {
+            total++;
+            final int expected = COMMERCIAL.equals(vect.getName()) ? 1 : 0;
+
+            final Vector collector = new DenseVector(2);
+            bestLearner.classifyFull(collector, vect);
+
+            final int cat = collector.maxValueIndex();
+            if (cat == expected) {
+                correct++;
+            }
+        }
+
+        final double cd = correct;
+        final double td = total;
+        System.out.println(cd / td);
+    }
+
+    // utils
+
+    private final List<Vector> vectors() throws IOException {
+        final List<NamedVector> namedVectors = namedVectors();
+        final List<Vector> vectors = Lists.<Vector> newArrayList(namedVectors);
+        return vectors;
+    }
+
+    private final List<NamedVector> namedVectors() throws IOException {
+        final NamedVector noncommercial1 = encode(NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("How to travel around the world for a year"));
+        final NamedVector noncommercial2 = encode(NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("What is nux and what's it used for?"));
+        final NamedVector noncommercial3 = encode(NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("What is the fastest way to learn #JPA? "));
+        final NamedVector noncommercial4 = encode(NONCOMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("How can I execute a script each time I log in and out from OS X?"));
+        final NamedVector commercial1 = encode(COMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("We're looking to #hire a Front End Developer/Creative Designer to join our team in Leeds. Get in touch for more information."));
+        final NamedVector commercial2 = encode(COMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("New job Nurse Practitioners & Physician Assistants - New Jersey  #hire #jobs"));
+        final NamedVector commercial3 = encode(COMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("Looking to hire excellent senior Java developers preferrably with MongoDb skills to our Espoo, Finland office #agile #hire #Java #MongoDb"));
+        final NamedVector commercial4 = encode(COMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("I'm looking for #scala dev #contract in #Melbourne or #Sydney. #job"));
+        final NamedVector commercial5 = encode(COMMERCIAL, Splitter.on(CharMatcher.anyOf(" ")).split("Scala Developer with World-Class Tech Co in Cambridge #job #scala #cambridge"));
+
+        final List<NamedVector> vectors = Lists.newArrayList(noncommercial1, commercial1, noncommercial2, commercial2, noncommercial3, commercial3, noncommercial4, commercial4, commercial5);
+        return vectors;
+    }
+
 }
