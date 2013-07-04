cf71c8f20e157c39f68e7861c42c2feeb504d023
eugenp <hanriseldon@gmail.com>
2013-07-04 14:11:08 +0300

    early work on tags separation by source

--- src/main/java/org/stackexchange/util/StackTagAdvanced.java
+++ src/main/java/org/stackexchange/util/StackTagAdvanced.java
@@ -1,6 +1,6 @@
 package org.stackexchange.util;
 
-public final class TagAdvanced {
+public final class StackTagAdvanced {
     public static final String amazons3 = "amazon-s3";
     public static final String amazonec2 = "amazon-ec2";
     public static final String amazonwebservices = "amazon-web-services";
@@ -8,7 +8,7 @@ public final class TagAdvanced {
     public static final String rubyonrails = "ruby-on-rails";
     public static final String objectivec = "objective-c";
 
-    private TagAdvanced() {
+    private StackTagAdvanced() {
         throw new AssertionError();
     }
 
--- src/test/java/org/stackexchange/service/TwitterHashtagWordsIntegrationTest.java
+++ src/test/java/org/stackexchange/service/TwitterHashtagWordsIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.stackexchange.spring.StackexchangeConfig;
-import org.stackexchange.util.TwitterTag;
+import org.stackexchange.util.StackTag;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { StackexchangeConfig.class })
@@ -22,7 +22,7 @@ public class TwitterHashtagWordsIntegrationTest {
 
     @Test
     public final void whenRetrievingMinStackExchangeScoreForTag_thenFound() {
-        for (final TwitterTag tag : TwitterTag.values()) {
+        for (final StackTag tag : StackTag.values()) {
             assertNotNull("No min score for tag " + tag, env.getProperty(tag.name() + ".minscore"));
         }
     }
