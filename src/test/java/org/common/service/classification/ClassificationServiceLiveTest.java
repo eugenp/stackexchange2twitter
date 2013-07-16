bad483665878ed009968146a9fd8707ea3ec61ce
eugenp <hanriseldon@gmail.com>
2013-07-16 18:12:13 +0300

    classification work

--- src/test/java/org/common/service/classification/ClassificationServiceLiveTest.java
+++ src/test/java/org/common/service/classification/ClassificationServiceLiveTest.java
@@ -49,9 +49,10 @@ public class ClassificationServiceLiveTest {
         assertTrue(isCommercial);
     }
 
+    // 0.906,0.905,0.913,0.910,0.910 = ~0.909
     @Test
     public final void givenClassifierWasTrained_whenClassifyingTestData_thenResultsAreGood() throws IOException {
-        final int runs = 500;
+        final int runs = 1000;
         final List<Double> results = Lists.newArrayList();
         for (int i = 0; i < runs; i++) {
             final List<ImmutablePair<String, String>> testData = ClassificationData.commercialAndNonCommercialTweets();
@@ -67,9 +68,10 @@ public class ClassificationServiceLiveTest {
         System.out.println("Average Success Rate: " + mean);
     }
 
+    // 0.682,0.681,0.688, , = ~0.683
     @Test
     public final void givenClassifierWasTrained_whenClassifyingTestDataNew_thenResultsAreGood() throws IOException {
-        final int runs = 500;
+        final int runs = 1000;
         final List<Double> results = Lists.newArrayList();
         for (int i = 0; i < runs; i++) {
             final List<ImmutablePair<String, String>> testData = ClassificationData.commercialAndNonCommercialTweets();
