c0b027368506a26db6acd56fc5d261b85b95239a
eugenp <hanriseldon@gmail.com>
2013-07-25 16:25:30 +0300

    testing and overall context work

--- src/test/java/org/tweet/twitter/service/TagRetrieverServiceIntegrationTest.java
+++ src/test/java/org/tweet/twitter/service/TagRetrieverServiceIntegrationTest.java
@@ -6,17 +6,14 @@ import org.common.spring.CommonContextConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.stackexchange.util.TwitterAccountEnum;
 import org.tweet.spring.TwitterConfig;
-import org.tweet.spring.util.SpringProfileUtil;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { CommonContextConfig.class, TwitterConfig.class })
-// TODO: profile temporarily active - fix
-@ActiveProfiles(SpringProfileUtil.LIVE)
+// @ActiveProfiles(SpringProfileUtil.LIVE)
 public class TagRetrieverServiceIntegrationTest {
 
     @Autowired
