d279e470d57ea02860aecd28d18fc330f41c4e8b
eugenp <hanriseldon@gmail.com>
2013-07-29 17:15:47 +0300

    classification work, quota poller update

--- src/main/java/org/tweet/meta/component/TwitterQuotaStatusPoller.java
+++ src/main/java/org/tweet/meta/component/TwitterQuotaStatusPoller.java
@@ -13,6 +13,7 @@ import org.springframework.social.twitter.api.ResourceFamily;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.UserOperations;
 import org.springframework.stereotype.Component;
+import org.stackexchange.util.GenericUtil;
 import org.stackexchange.util.TwitterAccountEnum;
 import org.tweet.spring.util.SpringProfileUtil;
 import org.tweet.twitter.service.live.TwitterReadLiveService;
@@ -33,19 +34,18 @@ public class TwitterQuotaStatusPoller {
 
     // API
 
-    @Scheduled(cron = "0 0 */2 * * *")
+    @Scheduled(cron = "0 0 */1 * * *")
     public void checkTwitterApiQuotaOnAllAccounts() throws JsonProcessingException, InterruptedException {
-        logger.info("Starting to check status of Twitter API Quota");
+        logger.info("Starting to check status of Twitter API Quotas for 3 random accounts");
 
-        for (final TwitterAccountEnum twitterAccount : TwitterAccountEnum.values()) {
-            if (twitterAccount.isRt()) {
-                checkTwitterApiQuotaOnOneAccount(twitterAccount.name());
-                logger.info("Done checking Quota status twitterAccount= " + twitterAccount.name() + "; sleeping for 30 secs...");
-                Thread.sleep(1000 * 30 * 1); // 10 sec
-            }
-        }
+        final TwitterAccountEnum account1 = GenericUtil.pickOneGeneric(TwitterAccountEnum.values());
+        final TwitterAccountEnum account2 = GenericUtil.pickOneGeneric(TwitterAccountEnum.values());
+        final TwitterAccountEnum account3 = GenericUtil.pickOneGeneric(TwitterAccountEnum.values());
+        checkTwitterApiQuotaOnOneAccount(account1.name());
+        checkTwitterApiQuotaOnOneAccount(account2.name());
+        checkTwitterApiQuotaOnOneAccount(account3.name());
 
-        logger.info("Finished checking status of Twitter API Quota");
+        logger.info("Finished checking status of Twitter API Quotas for the 3 random accounts");
     }
 
     // util
@@ -67,7 +67,7 @@ public class TwitterQuotaStatusPoller {
         final Map<ResourceFamily, List<RateLimitStatus>> applicationRateLimitStatusMap = userOperations.getRateLimitStatus(ResourceFamily.APPLICATION);
         final List<RateLimitStatus> applicationRateLimitStatusList = applicationRateLimitStatusMap.values().iterator().next();
         final RateLimitStatus appRate = applicationRateLimitStatusList.get(0);
-        logger.warn("Application of account= {} - remaining API hits= {} out of= {}; reset in {} mins", twitterAccount, appRate.getRemainingHits(), appRate.getQuarterOfHourLimit(), (appRate.getResetTimeInSeconds() / 216000000));
+        logger.warn("Application= {} - remaining API hits= {} out of= {}; reset in {} mins", twitterAccount, appRate.getRemainingHits(), appRate.getQuarterOfHourLimit(), (appRate.getResetTimeInSeconds() / 216000000));
 
         final Map<ResourceFamily, List<RateLimitStatus>> accountRateLimitStatusMap = userOperations.getRateLimitStatus(ResourceFamily.ACCOUNT);
         final List<RateLimitStatus> accountRateLimitStatusList = accountRateLimitStatusMap.values().iterator().next();
