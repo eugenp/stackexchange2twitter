65679c1d9eb30c95220746309bff6188a2890ebf
eugenp <hanriseldon@gmail.com>
2013-08-04 15:25:31 +0300

    major cleanup of tweet text retrieval mechanism

---
+++ src/main/java/org/tweet/twitter/util/TweetUtil.java
@@ -0,0 +1,25 @@
+package org.tweet.twitter.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.social.twitter.api.Tweet;
+
+public final class TweetUtil {
+    final static Logger logger = LoggerFactory.getLogger(TweetUtil.class);
+
+    private TweetUtil() {
+        throw new AssertionError();
+    }
+
+    // API
+
+    public static String getText(final Tweet tweet) {
+        final String text = tweet.getText();
+        // if (tweet.getRetweetedStatus() != null) {
+        // text = tweet.getRetweetedStatus().getText();
+        // }
+
+        return text;
+    }
+
+}
