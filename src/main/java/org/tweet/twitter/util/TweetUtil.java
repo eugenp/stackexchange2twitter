f45600be07b46fcf0a3fa56be74c3250119684d5
eugenp <hanriseldon@gmail.com>
2013-09-22 19:26:41 +0300

    tunning work

--- src/main/java/org/tweet/twitter/util/TweetUtil.java
+++ src/main/java/org/tweet/twitter/util/TweetUtil.java
@@ -15,21 +15,21 @@ public final class TweetUtil {
     /*
      * "ar"    - |
      * "ca"    - |
-     * "cs"    - +1 | 
+     * "cs"    - +1 +1 | 
      * "el"    - |
-     * "es"    - +1 +1 +1 +1 +1 +1 +1 | -1 -1 -1 -1 -1 -1 -1 -1 -1 - rejected
-     * "fr"    - +1 +1 +1 +1 +1 +1 +1 +1 +1 | at the full user level (not English): ~0 -1
+     * "es"    - +1 +1 +1 +1 +1 +1 +1 +1 | -1 -1 -1 -1 -1 -1 -1 -1 -1 - rejected
+     * "fr"    - +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 | at the full user level (not English): ~0 -1
      * "hu"    - +1 | 
-     * "it"    - +1 +1 +1 +1 +1 |
+     * "it"    - +1 +1 +1 +1 +1 +1 | -1
      * "id"    - | -1 -1 
      * "ja"    - +1 +1 +1 | 
      * "lv"    - +1 |
      * "ko"    - | -1 
      * "no"    - +1
-     * "nl"    - +1 | -1
-     * "pt"    - +1 +1 +1 +1 +1 | 
+     * "nl"    - +1 +1 | -1
      * "pl"    - | -1 
-     * "ru"    - +1 | 
+     * "pt"    - +1 +1 +1 +1 +1 +1 +1 | 
+     * "ru"    - +1 +1 | 
      * "sl"    - |
      * "sv"    - | -1
      * "sk"    - |
@@ -97,34 +97,35 @@ public final class TweetUtil {
      * "et"    - ~20 +1 - accepting
      * "el"    - | -1 -1 -1 | +1 - rejecting
      * -"fi"   - ~10 +1 | ~10 -1 - rejecting
-     * "fa"    - | -1 -1
-     * "hi"    - | -1 -1 -1 -1 -1 -1 -1  - rejecting
-     * "hu"    - +1 +1 +1 | -1 -1 -1
+     * "fa"    - | -1 -1 -1
+     * "hi"    - | -1 -1 -1 -1 -1 -1 -1 - rejecting
+     * "hu"    - +1 +1 +1 | -1 -1 -1 -1
      * "ht"    - +1 | -1 -1 -1 -1 - rejecting // some English but plain weird tweets
      * "it"    - 
-     * "is"    - +1 +1 | -1
+     * "is"    - +1 +1 +1 | -1
      * "id"    - 
      * "ja"    - | ~12 -1 - rejecting
      * "ko"    - +1 | -1 -1 -1 -1 - rejecting
      * "lt"    - ~10 +1 | ~10 -1 - rejecting
-     * "lv"    - +1 +1 +1 +1 +1 +1 | -1 -1 
+     * "lv"    - +1 +1 +1 +1 +1 +1 | -1 -1 -1 -1 -1 -1 -1 -1 -1 - rejecting
      * "ml"    - 
      * "nl"    - | -1 -1 -1 - rejecting
-     * "no"    - +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 | -1
+     * "no"    - 13 +1 | -1
      * "uk"    - | -1 -1 -1 -1 -1 -1 -1 - rejecting
      * "ur"    - | -1
      * -"pt"   - | -1 -1 -1 -1 = rejecting
      * "pl"    - ~25 | -1 -1 -1 -1  - reject for tweeting, accept for analysis
-     * "ru"    - 
+     * "ru"    - | 
      * -"sl"   - +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 +1 | -1 - accepting
-     * "sk"    - 
+     * "sk"    - | 
      * "ta"    - | -1 -1 -1 -1 -1 - rejected
+     * "th"    - | -1 
      * "tr"    - +1 +1 +1 | -1 -1 -1 -1 -1 -1 -1 - rejected
      * "tl"    - ~12 +1 | -1 -1 -1 -1 -1 - rejecting
-     * "und"   - 
-     * "xx-lc" -
+     * "und"   - | 
+     * "xx-lc" - | 
      * "vi"    - +1 +1 +1 +1 +1 +1 +1  = accepting
-     * "zh"    - -1 -1 
+     * "zh"    - | -1 -1 -1 
     */
     public final static List<String> acceptedTweetLangForAnalysis = Lists.newArrayList(
         "ca", 
@@ -155,6 +156,7 @@ public final class TweetUtil {
         "ja", 
         "ko", 
         "lt", 
+        "lv", 
         "nl", 
         "pt", 
         "sv", 
